75d26567772291a2a91d08af02a3b8c4f7cf130f
Edney Peterson Cicotoste <edney.cicotoste@freterapido.com>
2024-11-08 17:21:38 -0300

    projeto git na pratica

---
+++ start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Olá, Seja Bem Vindo!"
+
+echo "Qual é o seu nome?"
+read nome
+
+while true; do
+if [ ${#nome} -lt 2 ]; then
+    echo "O nome precisa ter pelo menos 2 caracteres. Por favor, digite novamente:"
+    read nome
+else
+    break
+fi
+done
+
+echo "Qual é a sua idade?"
+read idade
+
+while true; do
+if ! [[ "$idade" =~ ^[0-9]+$ ]]; then
+    echo "Valor informado inválido. Por favor, digite novamente:"
+    read idade
+else
+    break
+fi
+done
+
+echo "Usuário $nome informou ter $idade anos!"
