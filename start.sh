6d151d8d3b606bdfcad792c8ab043038d3f24ef4
Edney Peterson Cicotoste <edney.cicotoste@freterapido.com>
2024-11-09 13:05:04 -0300

    inclusao de diretorio log

--- start.sh
+++ start.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+diretorio="logs" 
+str_err_nome="O nome precisa ter pelo menos 2 caracteres, deve conter apenas letras e não pode ser nulo. Por favor, digite novamente: "
+str_err_idade="Valor informado inválido. Por favor, digite novamente: "
+
+if [ ! -d "$diretorio" ]; then
+    mkdir "$diretorio"
+    echo "O diretório '$diretorio' foi criado com sucesso."
+else
+    echo "O diretório '$diretorio' já existe."
+fi
+
+echo ""
 echo "Olá, Seja Bem Vindo!"
+echo ""
 
 echo "Qual é o seu nome?"
 read nome
 
 while true; do
 if [[ "${#nome}" -lt 2 || ! "$nome" =~ ^[a-zA-Z]+$ ]]; then
-    echo "O nome precisa ter pelo menos 2 caracteres, deve conter apenas letras e não pode ser nulo. Por favor, digite novamente:"
+  data_hora=$(date "+%Y-%m-%d %H:%M:%S")
+    echo "$str_err_nome"
+    echo "$data_hora -  Erro na digitação do nome: $nome" >> "$diretorio/log.txt"
     read nome
 else
     break
@@ -19,7 +34,8 @@ read idade
 
 while true; do
 if ! [[ "$idade" =~ ^[0-9]+$ ]]; then
-    echo "Valor informado inválido. Por favor, digite novamente:"
+    echo "$str_err_idade"
+    echo "$data_hora -  Erro na digitação da idade: $idade" >> "$diretorio/log.txt"
     read idade
 else
     break
